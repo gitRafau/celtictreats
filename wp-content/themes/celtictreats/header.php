18f6b7bb700190aab2da2893ad93c579364e2589
Rafal Fidurski <rafal.fidurski@gmail.com>
2018-09-11 13:47:41 +0200

    rwd header

--- wp-content/themes/celtictreats/header.php
+++ wp-content/themes/celtictreats/header.php
@@ -13,40 +13,40 @@
 	<body>
 			
 		<main class="container-fluid" style="background-image: url('<?php echo get_theme_file_uri('/img/nav_background.png'); ?>'); height: 200px; background-repeat: no-repeat; background-position: bottom;">
-	<div class="row">
-		<div class="col-md-2 col-lg-2">
-			<img src="<?php echo get_theme_file_uri('/img/celtic_logo.png'); ?>" alt="" height="120">
-		</div>
+		<div class="row">
+			<div class="col-md-2 col-lg-2">
+				<img src="<?php echo get_theme_file_uri('/img/celtic_logo.png'); ?>" alt="Logo Celtic Treats" height="120" class="logo_main">
+			</div>
 
-		<nav class="col-md-9 col-md-offset-1 col-lg-9">
-			<p class="navbar-text" style="margin-top: 1.2em;">
-				<a href="#" class="navbar-link">Home</a>
-			</p>
-			<p class="navbar-text" style="margin-top: 1.2em;">
-				<a href="#" class="navbar-link">About us</a>
-			</p>
-			<p class="navbar-text" style="margin-top: 1.2em;">
-				<a href="#" class="navbar-link">Beef</a>
-			</p>
-			<p class="navbar-text" style="margin-top: 1.2em;">
-				<a href="#" class="navbar-link">Chicken</a>
-			</p>
-			<p class="navbar-text" style="margin-top: 1.2em;">
-				<a href="#" class="navbar-link">Pork</a>
-			</p>
-			<p class="navbar-text" style="margin-top: 1.2em;">
-				<a href="#" class="navbar-link">Rabbit</a>
-			</p>
-			<p class="navbar-text" style="margin-top: 1.2em;">
-				<a href="#" class="navbar-link">Buy</a>
-			</p>
-			<p class="navbar-text" style="margin-top: 1.2em;">
-				<a href="#" class="navbar-link">Contact</a>
-			</p>
+			<nav class="col-md-9 col-md-offset-1 col-lg-9 col-lg-offset-1" style="background-color: red; height: 20px;">
+				<!--<p class="navbar-text" style="margin-top: 1.2em;">
+					<a href="#" class="navbar-link">Home</a>
+				</p>
+				<p class="navbar-text" style="margin-top: 1.2em;">
+					<a href="#" class="navbar-link">About us</a>
+				</p>
+				<p class="navbar-text" style="margin-top: 1.2em;">
+					<a href="#" class="navbar-link">Beef</a>
+				</p>
+				<p class="navbar-text" style="margin-top: 1.2em;">
+					<a href="#" class="navbar-link">Chicken</a>
+				</p>
+				<p class="navbar-text" style="margin-top: 1.2em;">
+					<a href="#" class="navbar-link">Pork</a>
+				</p>
+				<p class="navbar-text" style="margin-top: 1.2em;">
+					<a href="#" class="navbar-link">Rabbit</a>
+				</p>
+				<p class="navbar-text" style="margin-top: 1.2em;">
+					<a href="#" class="navbar-link">Buy</a>
+				</p>
+				<p class="navbar-text" style="margin-top: 1.2em;">
+					<a href="#" class="navbar-link">Contact</a>
+				</p>
 
-			<button class="btn btn-defaut phone" style="margin-top: 1em;">
-				<img src="<?php echo get_theme_file_uri('img/phone_icon.png'); ?>" height="21" class="pull-left">Order Now +353 (0) 087 395 60 20</button>
-
-		</nav>
+				<button class="btn btn-defaut phone" style="margin-top: 1em;">
+					<img src="<?php echo get_theme_file_uri('img/phone_icon.png'); ?>" height="21" class="pull-left">Order Now +353 (0) 087 395 60 20
+				</button>-->
+			</nav>
 	</div>
 </main>
