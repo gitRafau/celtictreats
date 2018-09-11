7bd93ade926c61492eb038d0a108f537b5477c5b
Rafal Fidurski <rafal.fidurski@gmail.com>
2018-09-11 23:19:57 +0200

    fixy

--- wp-content/themes/celtictreats/page-products.php
+++ wp-content/themes/celtictreats/page-products.php
@@ -5,21 +5,29 @@
 	<div class="col-md-3 bread_box">
 		<span class="breadcrumps"><?php the_breadcrumb(); ?></span>
 	</div>
-	<div class="col-md-5 col-md-offset-0">
-		<h1 class="contact">Where To Buy</h1>
+	<div class="col-md-6 col-md-offset-0">
+		<h1 class="contact">Wszystkie produkty</h1>
 	</div>
 	<div class="clearfix"></div>
 </div>
 </div>
 
-<div class="containter-fluid">
-
-<h1 class="text-center">Wszystkie Produkty</h1>
-	<div class="row">
-
-	</div>
+<section>
 	
-</div>
+	<div class="container" style="margin-top: 2em;">		
+		
+		<?php
+            while (have_posts()) {
+                the_post();
+                ?>
+
+                <?php the_content(); ?>
+
+            <?php }
+            ?>
+  							
+	</div>
+</section>
 
 <?php echo do_shortcode('[product id="63"]');?>
 
